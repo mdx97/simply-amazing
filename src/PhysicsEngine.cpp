03e75062300cd66c61285f0d67cdf533d2693617
mdx97 <mathewhorner123@gmail.com>
2020-09-25 16:59:20 -0500

    YES COLLISION

--- src/PhysicsEngine.cpp
+++ src/PhysicsEngine.cpp
@@ -4,8 +4,35 @@
 #include "PhysicsComponent.h"
 #include "PhysicsEngine.h"
 #include "SceneSystem.h"
-#include "Utility.h"
 
+struct Rectangle {
+    float x, y, w, h;
+};
+
+Rectangle EntityToWorldSpace(const CollisionComponent *collider)
+{
+    Rectangle rect;
+    rect.x = collider->entity->x + collider->x;
+    rect.y = collider->entity->y + collider->y;
+    rect.w = collider->w;
+    rect.h = collider->h;
+    return rect;
+}
+
+bool Collides(const CollisionComponent *collider1, const CollisionComponent *collider2)
+{
+    // @PERFORMANCE: Maybe find a way to precompute these?
+    // I mean it is a pretty simple computation, but with lots of elements it could be costly.
+    Rectangle rect1 = EntityToWorldSpace(collider1);
+    Rectangle rect2 = EntityToWorldSpace(collider2);
+
+    return (rect1.x <= rect2.x + rect2.w) && 
+           (rect1.x + rect1.w >= rect2.x) && 
+           (rect1.y <= rect2.y + rect2.h) && 
+           (rect1.y + rect1.h >= rect2.y);
+}
+
+// @TEMP: This entire function should be swapped out for angle of incidence calculation.
 void Flip(PhysicsComponent *phys)
 {
     phys->direction -= 180;
@@ -25,8 +52,7 @@ void PhysicsEngine::Tick()
                 auto *other = entities[j]->GetComponent<CollisionComponent>();
                 
                 if (other != nullptr) {
-                    if (Utility::Collides(entities[i], collider, entities[j], other)) {
-                        std::cout << "Collision detected!" << std::endl;
+                    if (Collides(collider, other)) {
                         auto *iPhys = entities[i]->GetComponent<PhysicsComponent>();
                         auto *jPhys = entities[j]->GetComponent<PhysicsComponent>();
 
--- src/Utility.cpp
+++ src/Utility.cpp
@@ -1,17 +1,5 @@
-#include <cmath>
-#include <iostream>
-#include "CollisionComponent.h"
 #include "Entity.h"
 #include "Utility.h"
-
-struct Point {
-    float x, y;
-};
-
-struct Rectangle {
-    Point a, b, c, d;
-    float w, h;
-};
 
 const float PI = 3.14159265358979323846;
 
@@ -25,49 +13,4 @@ void Utility::CenterEntity(Entity *entity, SDL_Surface *container, int width, in
 float Utility::DegreesToRadians(float degrees)
 {
     return degrees * PI / 180.0;
-}
-
-Rectangle ColliderToRectangle(const Entity *entity, const CollisionComponent *collider)
-{
-    Rectangle rect;
-    rect.a = { entity->x + collider->x, entity->y + collider->y };
-    rect.b = { rect.a.x + collider->w, rect.a.y };
-    rect.c = { rect.b.x, rect.a.y + collider->h };
-    rect.d = { rect.a.x, rect.c.y };
-    rect.w = collider->w;
-    rect.h = collider->h;
-    return rect;
-}
-
-bool FloatsEqual(float float1, float float2)
-{
-    //std::cout << float1 << " " << float2 << std::endl;
-    return std::fabs(float1 - float2) < 0.001;
-}
-
-int TriangleArea(const Point a, const Point b, const Point c)
-{
-    return std::fabs(((a.x * (b.y - c.y)) + (b.x * (c.y - a.y)) + (c.x * (a.y - b.y))) / 2);
-}
-
-bool PointInsideRectangle(const Point point, const Rectangle rect)
-{
-    int total_area = TriangleArea(point, rect.a, rect.b) + TriangleArea(point, rect.b, rect.c) + TriangleArea(point, rect.c, rect.d) + TriangleArea(point, rect.d, rect.a);
-    return FloatsEqual(total_area, rect.w * rect.h);
-}
-
-bool Utility::Collides(const Entity *entity1, const CollisionComponent *collider1, const Entity *entity2, const CollisionComponent *collider2)
-{
-    Rectangle rect1 = ColliderToRectangle(entity1, collider1);
-    Rectangle rect2 = ColliderToRectangle(entity2, collider2);
-
-    Point points[4] = { rect1.a, rect1.b, rect1.c, rect1.d };
-
-    for (auto point : points) {
-        if (PointInsideRectangle(point, rect2)) {
-            return true;
-        }
-    }
-
-    return false;
 }
